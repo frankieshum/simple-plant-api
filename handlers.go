1de094b8ed78a48845ff34135f6423ae5fead9a1
Frankie Shum <frankie.s_712@hotmail.co.uk>
2022-01-02 20:21:36 +0000

    Use json decoder instead of read & unmarshal

--- handlers.go
+++ handlers.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -19,7 +18,7 @@ func (api *Api) listPlants(w http.ResponseWriter, r *http.Request) {
 	plants, err := api.DB.GetAllPlants()
 	if err != nil {
 		log.Printf("Error: %v\n", err)
-		writeError(w, 500, "An error occurred while processing the request")
+		writeErrorResponse(w, 500, "An error occurred while processing the request")
 		return
 	}
 	writeResponse(w, 200, plants)
@@ -32,7 +31,7 @@ func (api *Api) getPlant(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		log.Printf("Plant Id '%v' is not an integer", id)
-		writeError(w, 400, "The Plant id must be an integer")
+		writeErrorResponse(w, 400, "The Plant id must be an integer")
 		return
 	}
 
@@ -40,11 +39,11 @@ func (api *Api) getPlant(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if errors.Is(err, &NotFoundError{}) {
 			log.Println("The specified Plant was not found")
-			writeError(w, 404, "The specified Plant was not found")
+			writeErrorResponse(w, 404, "The specified Plant was not found")
 			return
 		}
 		log.Printf("Error: %v\n", err)
-		writeError(w, 500, "An error occurred while processing the request")
+		writeErrorResponse(w, 500, "An error occurred while processing the request")
 		return
 	}
 	writeResponse(w, 200, plant)
@@ -53,24 +52,18 @@ func (api *Api) getPlant(w http.ResponseWriter, r *http.Request) {
 func (api *Api) postPlant(w http.ResponseWriter, r *http.Request) {
 	log.Printf("POST %v\n", r.RequestURI)
 
-	// Read body and try parse payload into Plant
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("An error occurred while reading the request body: %v\n", err)
-		writeError(w, 500, "An error occurred while processing the request")
-		return
-	}
+	// Read body and parse into Plant
 	plantRequest := PlantRequest{}
-	if err = json.Unmarshal(body, &plantRequest); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&plantRequest); err != nil {
 		log.Printf("The request body could not be parsed into a Plant: %v", err)
-		writeError(w, 400, "The request payload could not be parsed into a Plant")
+		writeErrorResponse(w, 400, "The request payload could not be parsed into a Plant")
 		return
 	}
 
 	// Validate the request
 	if validationResults := plantRequest.Validate(); len(validationResults) > 0 {
 		log.Println("The Plant request is invalid: ", strings.Join(validationResults, "; "))
-		writeError(w, 400, strings.Join(validationResults, "; "))
+		writeErrorResponse(w, 400, strings.Join(validationResults, "; "))
 		return
 	}
 
@@ -81,16 +74,16 @@ func (api *Api) postPlant(w http.ResponseWriter, r *http.Request) {
 		Light:      plantRequest.Light,
 		Water:      plantRequest.Water,
 	}
-	if err = api.DB.CreatePlant(newPlant); err != nil {
+	if err := api.DB.CreatePlant(newPlant); err != nil {
 		var conflictErr *ConflictError
 		if errors.As(err, &conflictErr) {
 			errMsg := fmt.Sprintf("Plant with %v '%v' already exists", conflictErr.ConflictingKey, conflictErr.ConflictingValue)
 			log.Println(errMsg)
-			writeError(w, 409, errMsg)
+			writeErrorResponse(w, 409, errMsg)
 			return
 		}
 		log.Printf("Error: %v\n", err)
-		writeError(w, 500, "An error occurred while processing the request")
+		writeErrorResponse(w, 500, "An error occurred while processing the request")
 		return
 	}
 	writeResponse(w, 201, map[string]string{})
@@ -99,17 +92,11 @@ func (api *Api) postPlant(w http.ResponseWriter, r *http.Request) {
 func (api *Api) putPlant(w http.ResponseWriter, r *http.Request) {
 	log.Printf("PUT %v\n", r.RequestURI)
 
-	// Read body and try parse payload into Plant
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("An error occurred while reading the request body: %v\n", err)
-		writeError(w, 500, "An error occurred while processing the request")
-		return
-	}
+	// Read body and parse into Plant
 	plantRequest := PlantRequest{}
-	if err = json.Unmarshal(body, &plantRequest); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&plantRequest); err != nil {
 		log.Printf("The request body could not be parsed into a Plant: %v", err)
-		writeError(w, 400, "The request payload could not be parsed into a Plant")
+		writeErrorResponse(w, 400, "The request payload could not be parsed into a Plant")
 		return
 	}
 
@@ -117,12 +104,12 @@ func (api *Api) putPlant(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		log.Printf("Plant Id '%v' is not an integer", id)
-		writeError(w, 400, "The Plant id must be an integer")
+		writeErrorResponse(w, 400, "The Plant id must be an integer")
 		return
 	}
 	if validationResults := plantRequest.Validate(); len(validationResults) > 0 {
 		log.Println("The Plant request is invalid: ", strings.Join(validationResults, "; "))
-		writeError(w, 400, strings.Join(validationResults, "; "))
+		writeErrorResponse(w, 400, strings.Join(validationResults, "; "))
 		return
 	}
 
@@ -139,11 +126,11 @@ func (api *Api) putPlant(w http.ResponseWriter, r *http.Request) {
 		if errors.As(err, &conflictErr) {
 			errMsg := fmt.Sprintf("Plant with %v '%v' already exists", conflictErr.ConflictingKey, conflictErr.ConflictingValue)
 			log.Println(errMsg)
-			writeError(w, 409, errMsg)
+			writeErrorResponse(w, 409, errMsg)
 			return
 		}
 		log.Printf("Error: %v\n", err)
-		writeError(w, 500, "An error occurred while processing the request")
+		writeErrorResponse(w, 500, "An error occurred while processing the request")
 		return
 	}
 	writeResponse(w, 200, map[string]string{})
@@ -156,13 +143,13 @@ func (api *Api) deletePlant(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		log.Printf("Plant Id '%v' is not an integer", id)
-		writeError(w, 400, "The Plant id must be an integer")
+		writeErrorResponse(w, 400, "The Plant id must be an integer")
 		return
 	}
 
 	if err := api.DB.DeletePlant(id); err != nil {
 		log.Printf("Error: %v\n", err)
-		writeError(w, 500, "An error occurred while deleting the plant")
+		writeErrorResponse(w, 500, "An error occurred while deleting the plant")
 		return
 	}
 	writeResponse(w, 204, map[string]string{})
@@ -174,7 +161,7 @@ func writeResponse(w http.ResponseWriter, httpStatusCode int, responseBody inter
 	json.NewEncoder(w).Encode(responseBody)
 }
 
-func writeError(w http.ResponseWriter, httpStatusCode int, errorMessage string) {
+func writeErrorResponse(w http.ResponseWriter, httpStatusCode int, errorMessage string) {
 	resp := ErrorResponse{
 		Error: errorMessage,
 	}
